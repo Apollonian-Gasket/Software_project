6ceb039e2cc2877cd38bafe04ef35541a308c794
A-V-BHAVYA <avbhavya.07@gmail.com>
2020-08-25 12:02:05 +0530

    webpage redirection

--- app/src/main/java/com/example/halfadozen/MainActivity.java
+++ app/src/main/java/com/example/halfadozen/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.halfadozen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+    WebView webview;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        webview=findViewById(R.id.webView);
+        WebSettings webSettings=webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webview.loadUrl("https://drkalamdonation.netlify.app/");
+       // webview.setWebViewClient(new WebViewClient());
+    }
+    @Override
+    public void onBackPressed() {
+        if(webview.canGoBack())
+        {
+            webview.goBack();
+        }
+        else {
+            super.onBackPressed();
+        }
     }
 }
---
+++ app/src/main/java/com/example/halfadozen/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.example.halfadozen;
+
+
+import android.content.Intent;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splash_activity);
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
+    }
+}
